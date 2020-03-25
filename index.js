e1e9c3901a822e7b545be951dc179c6906bcdd18
alinetenorio <aline.tenorio96@gmail.com>
2020-03-24 22:37:19 -0300

    config knex

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+app.post('/', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
     return response.json({
-        "hello": "world",
+        "hello": "earth",
     });
 });
 
