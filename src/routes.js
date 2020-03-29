e9d6328514cf2eef931f8df2031d3d55750e52df
alinetenorio <aline.tenorio96@gmail.com>
2020-03-29 15:03:46 -0300

    validation with celebrate

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const {celebrate, Segments, Joi} = require('celebrate');
+
 const NgoController = require('./controllers/NgoController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -11,15 +13,41 @@ routes.post('/session', SessionController.store);
 
 //NGOS
 routes.get('/ngo', NgoController.index);
-routes.post('/ngo', NgoController.store);
+
+
+routes.post('/ngo', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), NgoController.store);
 
 //INCIDENTS 
-routes.get('/incident', IncidentController.index);
+routes.get('/incident', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}),IncidentController.index);
+
+
 routes.post('/incident', IncidentController.store);
-routes.delete('/incident/:id', IncidentController.destroy);
+
+
+routes.delete('/incident/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}),IncidentController.destroy);
 
 //NGO PROFILE
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown()
+}),ProfileController.index);
 
 module.exports = routes;
 
